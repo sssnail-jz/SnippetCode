7455c18aa96fe070c9f1bc2d8628388ca5c8a291
郑建文 <1051975984@qq.com>
2021-12-18 11:38:51 +0800

    完成文章区域和右边信息区域的布局，使用了四个 flex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,94 @@
 <template>
   <el-container>
     <el-header></el-header>
-    <el-main></el-main>
+    <el-main>
+      <el-row class="row-bg" justify="center" :gutter="20">
+        <el-col :span="2"><div class="grid-content bg-margin"></div></el-col>
+        <el-col :span="14"
+          ><div class="grid-content bg-content">
+            <ul class="ul-content">
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+            </ul>
+          </div></el-col
+        >
+        <el-col :span="6"
+          ><div class="grid-content bg-aside">
+            <ul class="ul-content">
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+              <li>hello World</li>
+            </ul>
+          </div></el-col
+        >
+        <el-col :span="2"><div class="grid-content bg-margin"></div></el-col>
+      </el-row>
+    </el-main>
     <el-footer></el-footer>
   </el-container>
 </template>
@@ -22,5 +109,26 @@ export default {}
 }
 .el-main {
   background-color: #eaedf1;
+}
+
+.el-row {
+  height: 100%;
+}
+.el-col {
+  border-radius: 4px;
+  padding-bottom: 40px;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.bg-content,
+.bg-aside {
+  background: #d3dce6;
+}
+
+.ul-content {
+  margin: 0;
+  list-style: none;
 }
 </style>
