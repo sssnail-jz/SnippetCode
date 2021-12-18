337c844439311e49e3e29ea98e8e64d8ef0f0d96
郑建文 <1051975984@qq.com>
2021-12-18 10:56:46 +0800

    让 header、main、footer布满全屏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Hello snippet</div>
+  <el-container>
+    <el-header></el-header>
+    <el-main></el-main>
+    <el-footer></el-footer>
+  </el-container>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-footer {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
